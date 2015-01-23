f3f17224ebf8fd2614c4bc6ee9b2e33f796ef579
SuperValou <valentin.esmieu@insa-rennes.fr>
2015-01-23 16:39:21 +0100

    trying to integrate adtool again

--- Glasir/Glasir/ADToolHost.cs
+++ Glasir/Glasir/ADToolHost.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Interop;
@@ -9,6 +10,22 @@ namespace Glasir
 {
     public class ADToolHost : HwndHost
     {
+        [DllImport("user32.dll", EntryPoint = "CreateWindowEx", CharSet = CharSet.Unicode)]
+        internal static extern IntPtr CreateWindowEx(int dwExStyle,
+                                                      string lpszClassName,
+                                                      string lpszWindowName,
+                                                      int style,
+                                                      int x, int y,
+                                                      int width, int height,
+                                                      IntPtr hwndParent,
+                                                      IntPtr hMenu,
+                                                      IntPtr hInst,
+                                                      [MarshalAs(UnmanagedType.AsAny)] object pvParam);
+
+        [DllImport("user32.dll", EntryPoint = "DestroyWindow", CharSet = CharSet.Unicode)]
+        internal static extern bool DestroyWindow(IntPtr hwnd);
+
+
         IntPtr hwndControl;
         IntPtr hwndHost;
         int hostHeight, hostWidth;
@@ -23,6 +40,15 @@ namespace Glasir
         WS_BORDER = 0x00800000;
 
         /// <summary>
+        /// Le HWND du contrôle est exposé via une propriété en lecture seule, afin que la page hôte puisse l'utiliser pour envoyer des messages au contrôle.
+        /// </summary>
+        public IntPtr hwndListBox
+        {
+            get { return hwndControl; }
+        }
+
+
+        /// <summary>
         /// constructor
         /// </summary>
         /// <param name="height"></param>
@@ -31,6 +57,65 @@ namespace Glasir
         {
             hostHeight = (int)height;
             hostWidth = (int)width;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="hwndParent"></param>
+        /// <returns></returns>
+        protected override HandleRef BuildWindowCore(HandleRef hwndParent)
+        {
+            hwndControl = IntPtr.Zero;
+            hwndHost = IntPtr.Zero;
+
+            hwndHost = CreateWindowEx(0, "static", "",
+                                      WS_CHILD | WS_VISIBLE,
+                                      0, 0,
+                                      hostWidth, hostHeight,
+                                      hwndParent.Handle,
+                                      (IntPtr)HOST_ID,
+                                      IntPtr.Zero,
+                                      0);
+             
+            hwndControl = MainWindow.getADTool();
+
+            /*
+            hwndControl = CreateWindowEx(0, "ListBox", "",
+                                          WS_CHILD | WS_VISIBLE | LBS_NOTIFY
+                                            | WS_VSCROLL | WS_BORDER,
+                                          0, 0,
+                                          hostWidth, hostHeight,
+                                          hwndHost,
+                                          (IntPtr)LISTBOX_ID,
+                                          IntPtr.Zero,
+                                          0);
+            */
+            return new HandleRef(this, hwndHost);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="hwnd"></param>
+        /// <param name="msg"></param>
+        /// <param name="wParam"></param>
+        /// <param name="lParam"></param>
+        /// <param name="handled"></param>
+        /// <returns></returns>
+        protected override IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            handled = false;
+            return IntPtr.Zero;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="hwnd"></param>
+        protected override void DestroyWindowCore(HandleRef hwnd)
+        {
+            DestroyWindow(hwnd.Handle);
         }
     }
 }
--- Glasir/Glasir/MainWindow.xaml.cs
+++ Glasir/Glasir/MainWindow.xaml.cs
@@ -37,12 +37,11 @@ namespace Glasir
             process.StartInfo.FileName = "ADTool-1.4-jar-with-dependencies.jar";
             process.StartInfo.WorkingDirectory = "C:\\Users\\Valentin\\korrigolo\\Glasir\\Glasir\\bin\\Debug";
             process.StartInfo.Arguments = "adtree3.adt";
+            IntPtr pede = process.MainWindowHandle;
             process.Start();
             //int code = clientProcess.ExitCode != 0)
         }
 
-        public abstract class HwndHost : FrameworkElement, 
-	IDisposable, IWin32Window, IKeyboardInputSink
 
         /// <summary>
         /// when you drop (drag'n'drop)
@@ -54,6 +53,34 @@ namespace Glasir
 
         }
 
+        private void test(object sender, RoutedEventArgs e)
+        {
+            ADToolHost ADTool = new ADToolHost(ADToolZone.Height, ADToolZone.Width);
+            ADToolZone.Child = ADTool;
+        }
 
+        /*
+         *  Pour héberger le contrôle dans la page, vous créez tout d'abord une nouvelle instance de la classe ControlHost. Passez la hauteur et la largeur de l'élément de bordure qui contient le contrôle (ControlHostElement) au constructeur ControlHost. Cela garantit que la Zone de liste est dimensionnée correctement. Vous hébergez ensuite le contrôle dans la page en assignant l'objet ControlHost à la propriété Child de l'hôte Border. 
+         *  */
+
+
+
+
+        internal static IntPtr getADTool()
+        {
+            Process process = new Process();
+            process.StartInfo.FileName = "ADTool-1.4-jar-with-dependencies.jar";
+            process.StartInfo.WorkingDirectory = "C:\\Users\\Valentin\\korrigolo\\Glasir\\Glasir\\bin\\Debug";
+            process.StartInfo.Arguments = "adtree3.adt";
+            process.Start();
+            return process.MainWindowHandle;
+            
+            //int code = clientProcess.ExitCode != 0)
+        }
+
+        internal static void launchADToolMDFCKERS(Process process)
+        {
+            process.Start();
+        }
     }
 }
