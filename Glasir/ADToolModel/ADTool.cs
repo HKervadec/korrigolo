0e0ae2dcadaa3369000043b185600eaed8587fac
SuperValou <valentin.esmieu@insa-rennes.fr>
2015-02-04 15:33:40 +0100

    undo modelisation

---
+++ Glasir/ADToolModel/ADTool.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ADToolModel
+{
+    public class ADTool
+    {
+
+        public ADTree adtree
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public AddChildEdit addChildEdit
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public AddDomainEdit addDomainEdit
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public ChangeLabelEdit changeLabelEdit
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public UndoManager historyManager
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public void addChild()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void changeLabel()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void addDomain()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Glasir/ADToolModel/AddDomainEdit.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ADToolModel
+{
+    public class AddDomainEdit : UndoableEdit
+    {
+        private ADTree adt;
+    
+        public void execute()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void undo()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void redo()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void canUndo()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void canRedo()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
