233e6acdea6c84609eac9ec9429abdbc66364b46
SuperValou <valentin.esmieu@insa-rennes.fr>
2015-02-05 21:49:57 +0100

    draft of copy paste and fixing uml bug

---
+++ Glasir/ADToolModel/Clipboard.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ADToolModel
+{
+    public class Clipboard
+    {
+        public ClipBoardOwner owner
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public List<Transferable> contents
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+    }
+}
