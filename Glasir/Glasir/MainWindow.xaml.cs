cc2ea1b7814a8e68ba0a59feb888f3db27d8a029
SuperValou <valentin.esmieu@insa-rennes.fr>
2015-05-12 16:12:08 +0200

    fixing micro english mistake lol

--- Glasir/Glasir/MainWindow.xaml.cs
+++ Glasir/Glasir/MainWindow.xaml.cs
@@ -453,7 +453,7 @@ namespace Glasir
 
         private void helpADTool(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Files with .jar extention might be associate with something else than Java.\nPlease open the ReadMe file and follow the steps to change .jar file association.");
+            MessageBox.Show("Files with .jar extention might be associated with something else than Java.\nPlease open the ReadMe file and follow the steps to change .jar file association.");
         }
 
         
