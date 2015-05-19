92c8c8c957eb08145d8aee79eb1ec6eb69fd3533
SuperValou <valentin.esmieu@insa-rennes.fr>
2015-05-19 21:46:56 +0200

    rectificatifs rapports précédents

---
+++ Glasir/ADToolModel/ADTreeTransferHandler.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ADToolModel
+{
+    public class ADTreeTransferHandler
+    {
+        public int maxSize
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public ADTreeNode clonedNode
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+    
+        public void copy(ADTreeNode node)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void paste(ADTreeCanvas canvas)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Glasir/ADToolModel/UndoExecutor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ADToolModel
+{
+    public class UndoExecutor
+    {
+        public IEnumerable<ADTreeNode> queue
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public int memoryLimit
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public void undo()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void saveTreeState()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
